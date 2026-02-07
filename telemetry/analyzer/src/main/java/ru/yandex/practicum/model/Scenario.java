5e7b1481f69c0c6bb3457d239d00605345886b57
Denis Nagovitsyn <disochkin@gmail.com>
2026-02-07 20:09:16 +0000

    первая попытка

---
+++ telemetry/analyzer/src/main/java/ru/yandex/practicum/model/Scenario.java
@@ -0,0 +1,27 @@
+package ru.yandex.practicum.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(
+        name = "scenarios",
+        uniqueConstraints = @UniqueConstraint(columnNames = {"hub_id", "name"})
+)
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Scenario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "hub_id", nullable = false)
+    private String hubId;
+
+    @Column(nullable = false)
+    private String name;
+}
